88b3e8539c0565fe0af35556f1218ff646ea8043
LucasAlvesBueno <166718572+LucasAlvesBueno@users.noreply.github.com>
2024-08-13 10:24:49 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld { //cria uma classe public de nome HelloWorld mesmo nome do arquivo.
+
+    public static void main(String[] args) { //declara a classe inicializadora main
+
+        System.out.println("Oieee"); // chama a função System para utilizar uma de suas funcionalidades para imprimir a mensagem Oieee no console
+    }
+}
